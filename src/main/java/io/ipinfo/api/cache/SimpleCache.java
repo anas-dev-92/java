3bf695b17dfa27362f85b9c8ee8cfee79b697585
Anas Mohammed <Anas_Mohammed92@outlook.com>
2021-07-28 21:50:19 +0500

    get most of batch ops done.
    this required changes to cache.

--- src/main/java/io/ipinfo/api/cache/SimpleCache.java
+++ src/main/java/io/ipinfo/api/cache/SimpleCache.java
@@ -13,6 +13,7 @@ public class SimpleCache implements Cache {
     private final Duration duration;
     private final Map<String, Payload<ASNResponse>> asnCache = new HashMap<>();
     private final Map<String, Payload<IPResponse>> ipCache = new HashMap<>();
+    private final Map<String, Payload<Object>> cache = new HashMap<>();
 
     public SimpleCache(Duration duration) {
         this.duration = duration;
@@ -21,7 +22,9 @@ public class SimpleCache implements Cache {
     @Override
     public IPResponse getIp(String ip) {
         Payload<IPResponse> payload = ipCache.get(ip);
-        if (payload == null || payload.hasExpired()) return null;
+        if (payload == null || payload.hasExpired()) {
+            return null;
+        }
 
         return payload.data;
     }
@@ -29,7 +32,19 @@ public class SimpleCache implements Cache {
     @Override
     public ASNResponse getAsn(String asn) {
         Payload<ASNResponse> payload = asnCache.get(asn);
-        if (payload == null || payload.hasExpired()) return null;
+        if (payload == null || payload.hasExpired()) {
+            return null;
+        }
+
+        return payload.data;
+    }
+
+    @Override
+    public Object get(String key) {
+        Payload<Object> payload = cache.get(key);
+        if (payload == null || payload.hasExpired()) {
+            return null;
+        }
 
         return payload.data;
     }
@@ -47,14 +62,20 @@ public class SimpleCache implements Cache {
     }
 
     @Override
-    public boolean clear() {
-        ipCache.clear();
-        asnCache.clear();
+    public boolean set(String key, Object val) {
+        cache.put(key, new Payload<>(val, duration));
         return true;
     }
 
+    @Override
+    public boolean clear() {
+        ipCache.clear();
+        asnCache.clear();
+        cache.clear();
+        return true;
+    }
 
-    private class Payload<T> {
+    private static class Payload<T> {
         final T data;
         final Instant creation;
         final Duration expiration;
@@ -63,13 +84,11 @@ public class SimpleCache implements Cache {
             this.data = data;
             creation = Instant.now();
             this.expiration = duration;
-
         }
 
         public boolean hasExpired() {
             long time = expiration.addTo(creation).getLong(ChronoField.INSTANT_SECONDS);
             long now = System.currentTimeMillis();
-
             return now <= time;
         }
 
